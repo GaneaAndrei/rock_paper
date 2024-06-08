8e4e2f624aac6f8e2f3555bdf98ab5ef886c383d
odinstudent <andrei_marian_99@yahoo.com>
2024-06-09 00:38:26 +0300

    implemented the player buttons and checked their functionality with console

--- game.js
+++ game.js
@@ -1,7 +1,32 @@
-
-
 let playerScore=0;
 let computerScore=0;
+let playerChoice=''
+const rock=document.getElementById('rock')
+const paper=document.getElementById('paper')
+const scissors=document.getElementById('scissors')
+const container=document.getElementById("display")
+
+rock.addEventListener('click',()=>{
+    playerChoice="rock"
+    console.log("player a ales",playerChoice)
+    playRound(playerChoice);
+   
+   
+})
+
+paper.addEventListener('click',()=>{
+    playerChoice="paper"
+    console.log("player a ales",playerChoice)
+    playRound(playerChoice);
+   
+})
+scissors.addEventListener('click',()=>{
+    playerChoice="scissors"
+    console.log("player a ales",playerChoice)
+    playRound(playerChoice);
+
+    
+})
 
 function getComputerChoice()
 {
@@ -10,11 +35,12 @@ function getComputerChoice()
     return choices[random];
 }
 
-function playRound()
+function playRound(playerChoice)
 { 
  
-    let playerChoice=prompt("Enter your choice:").toLowerCase();
+
     let computerSelection=getComputerChoice();
+    console.log("computer a ales",computerSelection);
     if(computerSelection==playerChoice)
     {
         let message="Tie";
@@ -55,12 +81,12 @@ function playRound()
         playerScore++;
         return message;
     }
-    
+
 }
 
 
 
-function showWinner()
+/*function showWinner()
 {
     if(computerScore>playerScore)
     {
@@ -71,21 +97,4 @@ function showWinner()
         console.log("You won the match");
     }
 }
-
-function playGame()
-{
-    console.log(playRound());
-if(playerScore<5 && computerScore<5)
-{
-    playGame();
-}
-else
-{
-    showWinner();
-}
-
-
-}
-playGame();
-
-
+*/
